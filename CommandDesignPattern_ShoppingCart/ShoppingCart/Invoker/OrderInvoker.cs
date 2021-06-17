3fc1705cae29635780bfdbdfa3fdd32c2bc3033d
Chase <chasestruse@gmail.com>
2021-06-16 20:15:03 -0500

    Added invoker

---
+++ CommandDesignPattern_ShoppingCart/ShoppingCart/Invoker/OrderInvoker.cs
@@ -0,0 +1,22 @@
+﻿using ShoppingCart.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Invoker
+{
+    public class OrderInvoker
+    {
+        private IOrderCommand _orderCommand;
+
+        public void SetOrderCommand(IOrderCommand command) => _orderCommand = command;
+
+        public void Invoke()
+        {
+            _orderCommand.Execute();
+        }
+
+    }
+}
