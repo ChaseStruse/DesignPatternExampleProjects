7e83a18da468251a61a72cc9f8c65014c750c034
Chase <chasestruse@gmail.com>
2021-06-16 19:53:03 -0500

    Added necessary models

---
+++ CommandDesignPattern_ShoppingCart/ShoppingCart/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Models
+{
+    public class Order
+    {
+        public List<Item> OrderItems { get; set; } = new List<Item>();
+    }
+}
