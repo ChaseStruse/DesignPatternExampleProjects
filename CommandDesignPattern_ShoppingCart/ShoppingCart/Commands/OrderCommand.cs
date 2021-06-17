38445dac5b188a8bc9aec977a8d82b413531bb06
Chase <chasestruse@gmail.com>
2021-06-16 19:44:43 -0500

    Push file

---
+++ CommandDesignPattern_ShoppingCart/ShoppingCart/Commands/OrderCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Commands
+{
+    public interface IOrderCommand
+    {
+        void Execute();
+    }
+    public class OrderCommand : IOrderCommand
+    {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
