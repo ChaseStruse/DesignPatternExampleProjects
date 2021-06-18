2baa5ea1b86a481c55e626cf62e057295d04672d
Chase <chasestruse@gmail.com>
2021-06-17 20:28:50 -0500

    Got lifetime stuff working

--- BridgeDesignPattern_MovieLicense/Program.cs
+++ BridgeDesignPattern_MovieLicense/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BridgeDesignPattern_MovieLicense.Discounts;
+using BridgeDesignPattern_MovieLicense.Models;
+using BridgeDesignPattern_MovieLicense.MovieLicenses;
+using System;
 
 namespace BridgeDesignPattern_MovieLicense
 {
@@ -6,7 +9,20 @@ namespace BridgeDesignPattern_MovieLicense
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Movie movie = new Movie() { Name = "Avatar",  Genre = "Action"};
+            SeniorDiscount seniorDiscount = new SeniorDiscount();
+            LifetimeLicense lifetimeLicense = new LifetimeLicense(movie, seniorDiscount.GetDiscount(), DateTime.Now);
+
+            Console.WriteLine("Lifetime License Price Prior to Senior Discount: " + lifetimeLicense.GetCorePrice());
+            Console.WriteLine("Lifetime License Price After Senior Discount: " + lifetimeLicense.GetPrice());
+            Console.WriteLine("");
+
+            MilitaryDiscount militaryDiscount = new MilitaryDiscount();
+            LifetimeLicense lifetimeLicenseMilitaryDiscount = new LifetimeLicense(movie, militaryDiscount.GetDiscount(), DateTime.Now);
+
+            Console.WriteLine("Lifetime License Price Prior to Military Discount: " + lifetimeLicenseMilitaryDiscount.GetCorePrice());
+            Console.WriteLine("Lifetime License Price After Military Discount: " + lifetimeLicenseMilitaryDiscount.GetPrice());
+            Console.WriteLine("");
         }
     }
 }
