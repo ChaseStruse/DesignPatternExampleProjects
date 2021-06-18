e9de0d2056b838ded49966f59685f502ce3092f5
Chase <chasestruse@gmail.com>
2021-06-17 20:08:52 -0500

    Created model

---
+++ BridgeDesignPattern_MovieLicense/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeDesignPattern_MovieLicense.Models
+{
+    public class Movie
+    {
+        public String Name { get; set; }
+        public String Genre { get; set; }
+
+    }
+}
