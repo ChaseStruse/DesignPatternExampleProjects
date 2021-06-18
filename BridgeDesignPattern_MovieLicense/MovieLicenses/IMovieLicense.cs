c9e1b1b7d5632d9a05b255ae99026a36a24e91cb
Chase <chasestruse@gmail.com>
2021-06-17 20:07:42 -0500

    Created interface

---
+++ BridgeDesignPattern_MovieLicense/MovieLicenses/IMovieLicense.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeDesignPattern_MovieLicense.MovieLicenses
+{
+    public interface IMovieLicense
+    {
+        double GetCorePrice();
+        double GetPrice();
+        int GetExpriation();
+
+    }
+}
