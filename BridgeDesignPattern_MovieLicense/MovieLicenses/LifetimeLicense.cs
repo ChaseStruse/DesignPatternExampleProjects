d33a7e2041d119f09d7758667f21899caa8902eb
Chase <chasestruse@gmail.com>
2021-06-17 20:10:24 -0500

    Created new licenses

---
+++ BridgeDesignPattern_MovieLicense/MovieLicenses/LifetimeLicense.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeDesignPattern_MovieLicense.MovieLicenses
+{
+    public class LifetimeLicense : IMovieLicense
+    {
+        public double GetCorePrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetExpriation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
