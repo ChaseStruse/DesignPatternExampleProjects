1bcaf8bdfd74f42c3e5a3ab911a44134913ad59e
Chase <chasestruse@gmail.com>
2021-06-17 20:19:40 -0500

    Added discount interface

---
+++ BridgeDesignPattern_MovieLicense/Discounts/IDiscount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeDesignPattern_MovieLicense.Discounts
+{
+    interface IDiscount
+    {
+        double GetDiscount();
+    }
+}
