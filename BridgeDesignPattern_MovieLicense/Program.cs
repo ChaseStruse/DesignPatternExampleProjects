409a6a7bfda65213a772d9e55d09f8303ecb22ad
Chase <chasestruse@gmail.com>
2021-06-17 20:01:50 -0500

    Added new project for BridgeDesign

---
+++ BridgeDesignPattern_MovieLicense/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BridgeDesignPattern_MovieLicense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
